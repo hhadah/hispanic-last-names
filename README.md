7613b087871c0247636ee67a7ec23c7515811f9a
Hussain Hadah <hhadah@uh.edu>
2023-08-23 10:15:19 -0500

    add read me file

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# hispanic-last-names
-This is the repo for "The Impact of Hispanic Last Names and Identity on Labor Market Outcomes"
+# The Impact of Hispanic Last Names and Identity on Labor Market Outcomes
+
+This repository contains data and code for _The Impact of Hispanic Last Names and Identity on Labor Market Outcomes_.
+
+## Data
+
+The data for this project is from the Integrated Public Use Microdata Series (IPUMS) Current Population Survey (CPS) for the years 1994-2019. The data is available for download from the [IPUMS website](https://cps.ipums.org/cps/). The data is also available in the `data` folder of this repository. Also, I use Integrated Public Use Microdata Series (IPUMS) American Community Survey (ACS) for the years 1960-2000. The data is available for download from the [IPUMS website](https://usa.ipums.org/usa/). The data is also available in the `data` folder of this repository.
+
+Use the following citation to cite the data:
+
+Steven Ruggles, Sarah Flood, Matthew Sobek, Danika Brockman, Grace Cooper,  Stephanie Richards, and Megan Schouweiler. IPUMS USA: Version 13.0 [dataset]. Minneapolis, MN: IPUMS, 2023. https://doi.org/10.18128/D010.V13.0
+
+Sarah Flood, Miriam King, Renae Rodgers, Steven Ruggles, J. Robert Warren and Michael Westberry. Integrated Public Use Microdata Series, Current Population Survey: Version 10.0 [dataset]. Minneapolis, MN: IPUMS, 2022. https://doi.org/10.18128/D030.V10.0
+
+## Code
+To run the code:
+1. Download the data from the [IPUMS website](https://cps.ipums.org/cps/) and [IPUMS website](https://usa.ipums.org/usa/) or use the data in the `data` folder of this repository.
+2. Open the `code` folder of this repository.
+3. Open the `95-make-all.R` file.
+
+
--- programs/95-make-all.R
+++ programs/95-make-all.R
@@ -22,12 +22,32 @@ add_health_data_dir <- "/Users/hhadah/Dropbox/Research/My Research Data and Idea
 # pres_tabs <- paste0(mdir,"/presentations/jmp-presentation/tables")
 # pres_plots <- paste0(mdir,"/presentations/jmp-presentation/figures")
 
-dissertation_wd <- paste0("/Users/hhadah/Documents/GiT/my_thesis/presentation/dissertation")
+# dissertation_wd <- paste0("/Users/hhadah/Documents/GiT/my_thesis/presentation/dissertation")
 ### run do files and scripts
 
 # main scripts
 source(file.path(programs,"01-packages-wds.r")) # set up package
+stata(file.path(programs,"02-ACS-Synt-Parents-DoFile.do")) # run ACS do file
+stata(file.path(programs,"03-CleaningDataDoFile.do")) # clean data
+source(file.path(programs,"04-ACS-prep.r")) # prep data
+stata(file.path(programs,"05-CPS-Do-File.do")) # CPS Do File
+stata(file.path(programs,"06-CPS-DataCleanUpScript.do")) # CPS data cleaning
+# stata(file.path(programs,"07-BySexAnalysis.do")) # by sex analysis
+# stata(file.path(programs,"08-CoupleAnalysis.do")) # couple analysis
+# stata(file.path(programs,"09-Regressions.do")) # regressions
+source(file.path(programs,"10-merge-cps-acs.r")) # merge cps and acs
+source(file.path(programs,"11-table-one-parent-types.R")) # table one
+source(file.path(programs,"12-table-two-parent-types.R")) # table two
+source(file.path(programs,"13-table-three-summary-stats.R")) # table three
+# source(file.path(programs,"13a-table-three-summary-stats-mex.R")) # table three
+# source(file.path(programs,"13b-table-three-summary-stats-nonmex.R")) # table three
+source(file.path(programs,"14-table-three-summary-stats.R")) # table three
+source(file.path(programs,"15-table-four-summary-stats.R")) # table four
+source(file.path(programs,"16-table-five-regression.R")) # table five
+source(file.path(programs,"17-table-six-regression.R")) # table six
+source(file.path(programs,"18-table-seven-regression.R")) # table seven
+source(file.path(programs,"19-table-eight-regression.R")) # table eight
 
-# Send Message
+# Send Message when code is done
 
 textme(message = "👹 Back to work! You're not paid to run around and drink ☕ all day!")
